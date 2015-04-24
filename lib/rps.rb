24cf1ae71769b64463d644379f3ba840c1a61306
Frank <jedigene@gmail.com>
2015-04-23 20:34:54 -0700

    Refactored code.

--- lib/rps.rb
+++ lib/rps.rb
@@ -2,15 +2,20 @@ class String
 
   define_method(:beats) do
 
-    results = { "rocks tie" => 2, "paper wins" => 4, "papers tie" => 6, "rock wins" => 8, "scissors wins" => 10, "scissors tie" => 14 }
+    results = {
+      2 => "rocks tie",
+      4 => "paper wins",
+      6 => "papers tie",
+      8 => "rock wins",
+      10 => "scissors wins",
+      14 => "scissors tie"
+      }
 
     rps_values = { "rock" => 1, "paper" => 3, "scissors" => 7}
 
     score = rps_values.fetch(self) + rps_values.fetch("scissors")
 
-    inv_results = results.invert()
-
-    output = inv_results.fetch(score)
+    output = results.fetch(score)
 
     output
   end
